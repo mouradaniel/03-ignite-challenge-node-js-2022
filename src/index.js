786c17fe4aed163a88187c34e5f6e71333e37f7c
Daniel Moura <daniel-moura@outlook.com>
2023-01-21 17:29:21 -0300

    fix: findIndex and delete property likes before update

--- src/index.js
+++ src/index.js
@@ -32,11 +32,13 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
+  delete updatedRepository.likes
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
